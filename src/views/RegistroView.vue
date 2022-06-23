5786c7ccba6eff60c4d15f11d15310b721daff45
paulozemr <pauloramosj213@gmail.com>
2022-06-23 11:20:12 -0300

    site em dev 1

--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -8,46 +8,56 @@
         <div class="card rounded-3" >
           <div class= "card-body bg-dark text-white p-4 p-md-5"  style="border-radius: 1rem;">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Registre-se</h3>
-
             <form class="px-md-2">
-
               <div class="form-outline mb-4">
                 <input type="text" id="form3Example1q" class="form-control" />
                 <label class="form-label" for="form3Example1q">Nome</label>
               </div>
-
               <div class="row">
-                <div class="col-md-6 mb-4">
-
-                  <div class="form-outline datepicker">
-                    <input type="date" class="form-control" id="exampleDatepicker1" />
-                    <label for="exampleDatepicker1" class="form-label">Selecione sua data de nascimento</label>
-                  </div>
-
-                </div>
                 <div class="form-outline mb-4">
                 <input type= "email" id="form3Example1q" class="form-control" />
                 <label class="form-label" for="form3Example1q">E-mail</label>
               </div>
+                <div class="form-outline mb-4">
+                  <div class="form-outline datepicker">
+                    <input type= "email" id="form3Example1q" class="form-control" />
+                  <label class="form-label" for="form3Example1q">Senha</label>
+                  </div>
+                </div>
               </div>
 
-              <div class="mb-4">
-                <select class="select">
-                  <option value="1" disabled>Turma</option>
-                  <option value="2">1info1</option>
-                  <option value="3">1info2</option>
-                  <option value="4">1info3</option>
-                  <option value="5">2info1 s2</option>
-                  <option value="6">2info2</option>
-                  <option value="7">2info3</option>
-                  <option value="8">3info1</option>
-                  <option value="9">3info2</option>                                                                        
-                  <option value="10">3info3</option>                
-                </select>
-
+              <div class="row justify-content-between">
+                <div class="col-7">
+                <div class="form-outline datepicker">
+                    <input type="date" class="form-control" id="exampleDatepicker1" />
+                    <label for="exampleDatepicker1" class="form-label">Selecione sua data de nascimento</label>
+                  </div>
+                  </div>
+                <div class="col-3">
+                  <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      Turma
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li><a class="dropdown-item" href="#">1info1</a></li>
+                      <li><a class="dropdown-item" href="#">1info2</a></li>
+                      <li><a class="dropdown-item" href="#">1info3</a></li>
+                      <li><a class="dropdown-item" href="#">2info1</a></li>
+                      <li><a class="dropdown-item" href="#">2info2</a></li>
+                      <li><a class="dropdown-item" href="#">2info3</a></li>
+                      <li><a class="dropdown-item" href="#">3info1</a></li>
+                      <li><a class="dropdown-item" href="#">3info2</a></li>
+                      <li><a class="dropdown-item" href="#">3info3</a></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
 
               <button type="submit" class="btn btn-success btn-lg mb-1">Registrar</button>
+              <div class="row">
+              <p class="mb-0">Já possui conta?</p>
+              <RouterLink to="/login"><p class="text-white-50 fw-bold">Logar-se</p></RouterLink>
+            </div>
 
             </form>
 
