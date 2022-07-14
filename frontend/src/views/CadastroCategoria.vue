8e04168ce72903c2ddad93e46c62c2e66555a10e
paulozemr <pauloramosj213@gmail.com>
2022-07-14 11:48:02 -0300

    he

--- frontend/src/views/CadastroCategoria.vue
+++ frontend/src/views/CadastroCategoria.vue
@@ -1,97 +1,121 @@
 <script>
-import axios from "axios"
+import axios from "axios";
 export default {
-    data() {
-        return {
-            nova_cate: "",
-            descricacao: "",
-            categorias: [],
-        }
+  data() {
+    return {
+      nova_cate: "",
+      descricacao: "",
+      categorias: [],
+    };
+  },
+  async created() {
+    const categorias = await axios.get("http://localhost:4000/categorias");
+    this.categorias = categorias.data;
+  },
+  methods: {
+    async salvar() {
+      const categoria = {
+        catego: this.nova_cate,
+        desc: this.descricacao,
+      };
+      const categoria_criada = await axios.post(
+        "http://localhost:4000/categorias",
+        categoria
+      );
+      this.categorias.push(categoria_criada.data);
+      this.nova_cate = "";
+      this.descricacao = "";
     },
-    async created (){
-      const categorias = await axios.get("http://localhost:4000/categorias")
-      this.catetegorias = categorias.data;
+    async excluir(categoria) {
+      await axios.delete(`http://localhost:4000/categorias/${categoria.id}`);
+      const indice = this.categorias.indexOf(categoria);
+      this.categorias.splice(indice, 1);
     },
-    methods:{
-        async salvar() {
-            const categoria = {
-                catego: this.nova_cate,
-                desc: this.descricacao,
-            };
-            const categoria_criada = await axios.post("http://localhost:4000/categorias", categoria)
-            this.categorias.push(categoria_criada.data);
-            this.nova_cate = "";
-            this.descricacao = "";
-        },
-            async excluir(categoria) {
-            await axios.delete(`http://localhost:4000/categorias/${categoria.id}`);
-            const indice = this.categorias.indexOf(categoria);
-            this.categorias.splice(indice, 1);
-        },
   },
 };
-
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="col-12 text-center">
-            <div class="title">
-                <h2>Cadastro De Livros</h2>
-            </div>
-            <div class="input-group">
-                <input class="form-control" type="text" name="" id="" placeholder="Adicione sua Categoria"
-                    v-model="nova_cate" />
-                <input class="form-control" type="text" name="" id="" placeholder="Adicione sua Descrição"
-                    v-model="descricacao" />
-                <button class="btn btn-secondary" @click="salvar">Salvar</button>
-            </div>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Categoria</th>
-                        <th scope="col">Descrição</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="categoria in categorias" :key="categoria.id">
-                        <th>{{ categoria.id }}</th>
-                        <th>{{ categoria.catego }}</th>
-                        <th>{{ categoria.desc }} </th>
-                        <th><button class="btn btn-danger btn-sm" @click="excluir(categoria)">Excluir</button></th>
-                    </tr>
-                </tbody>
-            </table>
-       <iframe width="110" height="200" src="https://www.myinstants.com/instant/ohhh-my-god-26392/embed/" frameborder="0" scrolling="no"></iframe>
-       </div>
+  <div class="container-fluid">
+    <div class="col-12 text-center">
+      <div class="title">
+        <h2>Cadastro De Livros</h2>
+      </div>
+      <div class="input-group">
+        <input
+          class="form-control"
+          type="text"
+          name=""
+          id=""
+          placeholder="Adicione sua Categoria"
+          v-model="nova_cate"
+        />
+        <input
+          class="form-control"
+          type="text"
+          name=""
+          id=""
+          placeholder="Adicione sua Descrição"
+          v-model="descricacao"
+        />
+        <button class="btn btn-secondary" @click="salvar">Salvar</button>
+      </div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="categoria in categorias" :key="categoria.id">
+            <th>{{ categoria.id }}</th>
+            <th>{{ categoria.catego }}</th>
+            <th>{{ categoria.desc }}</th>
+            <th>
+              <button class="btn btn-danger btn-sm" @click="excluir(categoria)">
+                Excluir
+              </button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+      <iframe
+        width="110"
+        height="200"
+        src="https://www.myinstants.com/instant/ohhh-my-god-26392/embed/"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .input-group .btn {
-    background-color: #c614f3;
-    transition: 0.25s;
+  background-color: #c614f3;
+  transition: 0.25s;
 }
 
 .input-group .btn:hover {
-    background-color: #73048f;
+  background-color: #73048f;
 }
 
 .col-12 {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 
 .col-12 .input-group {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 
 .col-12 .table {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 
 thead tr th {
-    font-size: 24px;
+  font-size: 24px;
 }
 </style>
--- frontend/src/views/CadastroLivros.vue
+++ frontend/src/views/CadastroLivros.vue
@@ -1,128 +1,165 @@
 <script>
-import { v4 as uuid } from "uuid";
+import axios from "axios";
 export default {
   data() {
     return {
-      livro: "",
-      editora: "",
-      categoria: "",
-      preco: "",
-      autor:"",
-      livros: [
-        { id: 1, livro: "Harry Potter", editora: "Bloomsbury Publishing", categoria: "Fantasia", autor: "J.K. Rowling", preco: "25,00" },
-        { id: 2, livro: "1984", editora: "Companhia das Letras", categoria: "Ficção", autor: "George Orwell", preco: "29,99" },
-      ],
+      editoras: [],
+      autores: [],
+      categorias: [],
+      livro: {},
     };
   },
+  async created() {
+    await this.buscarTodosOsAutores();
+    await this.buscarTodosAsCategorias();
+    await this.buscarTodosAsEditoras();
+  },
   methods: {
-    salvar() {
-     if (this.livro !== "") {
-      const id = uuid();
-      this.livros.push({
-        id: id,
-        livro: this.livro,
-        editora: this.editora,
-        categoria: this.categoria,
-        autor: this.autor,
-        preco: this.preco,
-      });
-      this.livro = "";
-      this.editora = "";
-      this.categoria = "";
-      this.autor = "";
-      this.preco = "";
-      } else {
-      alert("Informe as coisas!") 
-      } 
-     },
-   excluir(livro) {
-      const indice = this.livros.indexOf(livro);
-      this.livros.splice(indice, 1);
+    async buscarTodosAsEditoras() {
+      const editoras = await axios.get("http://localhost:4000/editora");
+      this.editoras = editoras.data;
     },
-},
+    async buscarTodosAsCategorias() {
+      const categorias = await axios.get("http://localhost:4000/categorias");
+      this.categorias = categorias.data;
+    },
+    async buscarTodosOsAutores() {
+      const autores = await axios.get("http://localhost:4000/autores");
+      this.autores = autores.data;
+    },
+    async salvar() {
+      await axios.post("http://localhost:4000/cadastrolivros", this.livro);
+      await this.buscarTodosAsEditoras();
+    },
+  },
 };
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="col-12 text-center">
-            <div class="title">
-                <h2>Cadastro De Livros</h2>
-            </div>
-            <div class="input-group">
-                <input class="form-control" type="text" name="" id="" placeholder="Livro" v-model="livro" />
-                <input class="form-control" type="text" name="" id="" placeholder="Editora" v-model="editora" />
-                <select class="form-select" name="" id="" v-model="categoria">
-                    <option disabled value="">Categoria</option>
-                    <option>Horror</option>
-                    <option>Autoajuda</option>
-                    <option>Ação e aventura</option>
-                    <option>Biografia</option>
-                    <option>Fantasia</option>
-                    <option>Ficção científica</option>
-                    <option>Horror</option>
-                    <option>História</option>
-                    <option>Humor</option>
-                    <option>Infantil</option>
-                    <option>Romance</option>
-                    <option>Religião e Espiritualidade</option>
-                    <option>Tecnologia e Ciência</option>
-                </select>
-                <input class="form-control" type="text" name="" id="" placeholder="Autor" v-model="autor" />
-                <input class="form-control" type="number" name="" id="" placeholder="Preço" v-model="preco" />
-                <button class="btn btn-secondary" @click="salvar">Salvar</button>
-            </div>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Livro</th>
-                        <th scope="col">Editora</th>
-                        <th scope="col">Categoria</th>
-                        <th scope="col">Autor</th>
-                        <th scope="col">Preço(R$)</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="livro in livros" :key="livro.id">
-                        <th>{{ livro.livro }}</th>
-                        <th>{{ livro.editora }}</th>
-                        <th>{{ livro.categoria }}</th>
-                        <th>{{ livro.autor}}</th>
-                        <th>{{ livro.preco }}</th>
-                        <th><button class="btn btn-danger btn-sm" @click="excluir">Excluir</button></th>
-                    </tr>
-                </tbody>
-            </table>
-        <iframe width="110" height="200" src="https://www.myinstants.com/instant/vine-boom-sound-70972/embed/" frameborder="0" scrolling="no"></iframe>
-        </div>
+  <div class="container-fluid">
+    <div class="col-12 text-center">
+      <div class="title">
+        <h2>Cadastro De Livros</h2>
       </div>
+      <div class="input-group">
+        <input
+          class="form-control"
+          type="text"
+          name=""
+          id=""
+          placeholder="Livro"
+          v-model="livro"
+        />
+        <select
+          class="form-select"
+          name=""
+          placeholder="Categoria"
+          v-model="livro.categoria"
+        >
+          <option
+            v-for="categoria in categorias"
+            :key="categoria.id"
+            :value="categoria.id"
+          >
+            {{ categoria.catego }}
+          </option>
+        </select>
+        <select
+          class="form-select"
+          placeholder="Editora"
+          name=""
+          id=""
+          v-model="editoras"
+        >
+          <option
+            v-for="editora in editoras"
+            :key="editora.id"
+            :value="editora.id"
+          >
+            {{ editora.edit }}
+          </option>
+        </select>
+        <select
+          class="form-select"
+          placeholder="Autor"
+          name=""
+          id=""
+          v-model="autores"
+        >
+          <option v-for="autor in autores" :key="autor.id" :value="autor.id">
+            {{ autor.nome }}
+          </option>
+        </select>
+        <input
+          class="form-control"
+          type="number"
+          name=""
+          id=""
+          placeholder="Preço"
+          v-model="preco"
+        />
+        <button class="btn btn-secondary" @click="salvar">Salvar</button>
+      </div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Livro</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Editora</th>
+            <th scope="col">Autor</th>
+            <th scope="col">Preço(R$)</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="livro in livros" :key="livro.id">
+            <th>{{ livro.nome }}</th>
+            <th>{{ livro.editora }}</th>
+            <th>{{ livro.categoria }}</th>
+            <th>{{ livro.autor }}</th>
+            <th>{{ livro.preco }}</th>
+            <th>
+              <button class="btn btn-danger btn-sm" @click="excluir(livro)">
+                Excluir
+              </button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+      <iframe
+        width="110"
+        height="200"
+        src="https://www.myinstants.com/instant/vine-boom-sound-70972/embed/"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .input-group .btn {
-    background-color: #c614f3;
-    transition: 0.25s;
+  background-color: #c614f3;
+  transition: 0.25s;
 }
 
 .input-group .btn:hover {
-    background-color: #73048f;
+  background-color: #73048f;
 }
 
 .col-12 {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 
 .col-12 .input-group {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 
 .col-12 .table {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 
 thead tr th {
-    font-size: 24px;
+  font-size: 24px;
 }
-
 </style>
