7229820d3eb483974c89e974a4cdf5a033d54cb0
paulozemr <102665914+paulozemr@users.noreply.github.com>
2022-07-20 17:45:26 -0300

    a

--- frontend/src/views/CadastroEditora.vue
+++ frontend/src/views/CadastroEditora.vue
@@ -1,35 +1,36 @@
 <script>
-import axios from "axios"
+import EditorasApi from "@/api/editoras.js";
+const editorasApi = new EditorasApi();
 export default {
-    data() {
-        return {
-            novo_editor: "",
-            site_editora: "",
-            editoras: [],
-        };
-    },
-    async created() {
-        const editoras = await axios.get("http://localhost:4000/editoras");
-        this.editoras = editoras.data;
-    },
-    methods: {
-        async salvar() {
-          const editor = {
-            edit: this.novo_editor,
-            site: this.site_editora,
-        };
-        const editor_criado = await axios.post("http://localhost:4000/editoras", editor);
-        this.editoras.push(editor_criado.data);
-        this.novo_editor = "";
-        this.site_editora = "";
-        },
-        async excluir(editor) {
-                await axios.delete(`http://localhost:4000/editoras/${editor.id}`);
-                const indice = this.editoras.indexOf(editor);
-                this.editoras.splice(indice, 1);
-            },
-        },
+  data() {
+    return {
+      editora: {},
+      site_editora: {},
+      editoras: [],
     };
+  },
+  async created() {
+    this.editoras = await editorasApi.buscarTodasAsEditoras();
+  },
+  methods: {
+    async salvar() {
+      if (this.editoras.id) {
+        await editorasApi.atualizarEditora(this.editora);
+      } else {
+        await editorasApi.adicionarEditora(this.editora);
+      }
+      this.editoras = await editorasApi.buscarTodasAsEditoras();
+      (this.site_editora = {}), (this.editora = {});
+    },
+    async excluir(editora) {
+      await editorasApi.excluirEditora(editora.id);
+      this.editoras = await editorasApi.buscarTodasAsEditoras();
+    },
+    editar(editora) {
+      Object.assign(this.editora, editora);
+    },
+  },
+};
 </script>
 
 <template>
@@ -40,9 +41,9 @@ export default {
             </div>
             <div class="input-group">
                 <input class="form-control" type="text" name="" id="" placeholder="Adicione uma Editora"
-                    v-model="novo_editor" />
+                    v-model="editora.nome" />
                 <input class="form-control" type="text" name="" id="" placeholder="Adicione o site dela"
-                    v-model="site_editora" />
+                    v-model="editora.site_editora" />
                 <button class="btn btn-secondary" @click="salvar">Salvar</button>
             </div>
             <table class="table table-striped">
@@ -50,14 +51,16 @@ export default {
                     <tr>
                         <th scope="col">Editora</th>
                         <th scope="col">Site</th>
-                        <th scope="col">Ações</th>
+                        <th scope="col">Excluir</th>
+                        <th scope="col">Atualizar</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="editor in editoras" :key="editor.id">
-                        <th>{{ editor.edit }}</th>
-                        <th>{{ editor.site }} </th>
-                        <th><button class="btn btn-danger btn-sm" @click="excluir(editor)">Excluir</button></th>
+                    <tr v-for="editora in editoras" :key="editora.id">
+                        <th>{{ editora.nome }}</th>
+                        <th>{{ editora.site_editora }} </th>
+                        <th><button class="btn btn-danger btn-sm" @click="excluir(editora)">Excluir</button></th>
+                        <th><button class="btn btn-success btn-sm" @click="editar(editora)">Atualizar</button></th>
                     </tr>
                 </tbody>
             </table>
